a2e9bb19b26bf7526420afe911830ce57ab68798
Louis <louisrcouture@protonmail.com>
2020-12-10 20:16:36 -0500

    Initial Commit

---
+++ TimeNote ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeNote ios
+//
+//  Created by Louis Couture on 2020-12-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeNote ios/TimeNote_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeNote_iosApp.swift
+//  TimeNote ios
+//
+//  Created by Louis Couture on 2020-12-10.
+//
+
+import SwiftUI
+
+@main
+struct TimeNote_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
