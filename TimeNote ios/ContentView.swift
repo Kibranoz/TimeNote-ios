0fffe138f4fa12ae51d6cd98f4b1a60f38f3e55a
Louis <louisrcouture@protonmail.com>
2020-12-24 21:54:43 -0500

    régler le problème du share qui marche juste une fois

--- TimeNote ios/ContentView.swift
+++ TimeNote ios/ContentView.swift
@@ -5,17 +5,166 @@
 //  Created by Louis Couture on 2020-12-10.
 //
 
+//
+//  ContentView.swift
+//  Timenote
+//
+//  Created by Louis Couture on 2020-11-13.
+//
+
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State private var showingAlert:Bool = false;
+    @State var nomFichier:String = "";
+    @State var timenote:timeNote = timeNote();
+    @State var text:String = "";
+    @State var time:String = "";
+    @State var displayItem = -1;
+    @State var pauseOrPlayButton = "play.fill"
+    @State var title = "";
+    @State var hours:Int = 0;
+    @State var minutes:Int = 0;
+    @State var seconds:Int = 0;
+    @State private var isSheetPresented:Bool = false
+    //@State inout var test:String = "a"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            //Text("TimeNote").padding()
+            //TextField("Titre", text: $title)
+            Text(time).bold().font(.system(size: 50))
+            if (displayItem == 1){
+                timeAdjustView(displayItem: $displayItem, timenote: $timenote, hours: $hours, minutes: $minutes, seconds: $seconds, pauseOrPlayButton: $pauseOrPlayButton, time: $time)
+            }
+            HStack(spacing: 50.0){
+                Button(action: {
+                    displayItem = 1
+                }, label: {
+                    Image(systemName: "clock.arrow.circlepath")
+                        .font(.system(size: 40))
+                        
+                }).buttonStyle(PlainButtonStyle())
+                Button(action: {
+                    timenote.receiveText(_text: text)
+                    text = timenote.sendText()
+                }, label: {
+                    Image(systemName: "text.insert")
+                        .font(.system(size: 40))
+                }).buttonStyle(PlainButtonStyle())
+                Button(action: {
+                    if (timenote.getSiEnPause()){
+                    pauseOrPlayButton = "pause.fill"
+                    timenote.play()
+                    }
+                    else {
+                        pauseOrPlayButton = "play.fill"
+                        timenote.pause()
+                    }
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (Timer) in
+                        time = timenote.getStrTime()
+                    }
+                   
+                    
+                    
+                }, label: {
+                    Image(systemName: pauseOrPlayButton)
+                        .font(.system(size: 40))
+                })
+                .buttonStyle(PlainButtonStyle())
+                Button(action: {
+                    timenote.receiveText(_text: text)
+                    timenote.write(text: text, to: title)
+
+                    self.isSheetPresented = true;
+   
+                }, label: {
+                    Image(systemName: "square.and.arrow.up")
+                        .font(.system(size: 40))
+                }).buttonStyle(PlainButtonStyle())
+                .sheet(isPresented: $isSheetPresented)  {
+                    ActivityView(isSheetPresented: $isSheetPresented, activityItems: [self.text], applicationActivities: [])
+                    
+                }
+                
+ }
+
+            TextEditor(text: $text)
+                .font(.system(size: 19))
+            
+        
+            
+            
+            
+        }
+        
     }
+   
 }
 
+struct timeAdjustView:View{
+    @Binding var displayItem:Int;
+    @Binding var timenote:timeNote;
+    @Binding var hours:Int;
+    @Binding var minutes:Int;
+    @Binding var seconds:Int;
+    
+    @State var strHours = "0"
+    @State var strMinutes = "0"
+    @State var strSeconds = "0"
+    @Binding var pauseOrPlayButton:String;
+    @Binding var time:String
+    var body: some View{
+        VStack{
+        HStack{
+            TextField("HH", text : $strHours)
+                
+            Text(":")
+            TextField("MM", text : $strMinutes)
+            Text(":")
+            TextField("SS", text:$strSeconds)
+        }
+        Button(action: {
+            pauseOrPlayButton = "pause.fill"
+            timenote.play()
+
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (Timer) in
+                time = timenote.getStrTime()
+            }
+            hours = Int (strHours) ?? 0
+            minutes = Int (strMinutes) ?? 0
+            seconds = Int (strSeconds) ?? 0
+            timenote.adjustTime(_hours: hours, _minutes: minutes, _seconds: seconds)
+            displayItem = -1;
+
+        }, label: {
+            Text(NSLocalizedString("Enregistrer", comment: "save button"))
+        }).buttonStyle(PlainButtonStyle())
+        }.padding(.horizontal, 100)
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+struct ActivityView: UIViewControllerRepresentable {
+    @Binding var isSheetPresented:Bool;
+   var activityItems: [Any]
+    var
+    applicationActivities: [UIActivity]?
+    
+   func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityView>) -> UIActivityViewController {
+      let ac = UIActivityViewController(activityItems: activityItems,
+                               applicationActivities: applicationActivities)
+    ac.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, completed:
+                                        Bool, arrayReturnedItems: [Any]?, error: Error?) in
+        isSheetPresented = false;}
+    return ac;
+   }
+   func updateUIViewController(_ uiViewController: UIActivityViewController,
+                               context: UIViewControllerRepresentableContext<ActivityView>) {}
+   }
+
+
---
+++ TimeNote ios/logic.swift
@@ -0,0 +1,104 @@
+//
+//  logic.swift
+//  TimeNote ios
+//
+//  Created by Louis Couture on 2020-12-10.
+//
+
+import Foundation
+//
+//  logic.swift
+//  Timenote
+//
+//  Created by Louis Couture on 2020-11-13.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+class timeNote{
+    var text = "";
+    var time:Int = 0;
+    var timeBeginning = 0
+    var pauseBeginning = 0;
+    var enPause:Bool = true;
+    var begin = true;
+    
+    var pauseTime:Int = 0
+    init(){
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (Timer) in
+            
+                self.tick()
+    
+           
+        }
+    }
+    func getStrTime() -> String{
+        
+        var hr = Int(floor(Double(self.time/3600)));
+        var min = Int(floor(Double((self.time/60)%60)));
+        var sec = Int((self.time%60))
+        
+        var strtime:String = String(hr) + ":" + String(min) + ":" + String(sec)
+        return strtime
+    }
+    func play(){
+        if (self.begin){
+            self.timeBeginning = Int(NSDate().timeIntervalSince1970)
+            print(self.timeBeginning)
+            self.begin = false
+        }
+        if (pauseTime != 0){
+            var delta = self.pauseTime - self.pauseBeginning
+            self.timeBeginning += delta
+        }
+        self.enPause = false;
+    }
+    
+    func write(text: String, to fileNamed: String, folder: String = "TimenoteFiles") {
+        //let ac = UIActivityViewController(activityItems: ["hi"], applicationActivities: nil);
+        
+        //ac.popoverPresentationController?.barButtonItem = myBarButtonItem
+        //ac.isModalInPresentation = true;
+        //ac.present(ac, animated: true, completion: nil);
+        
+        guard let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else { return }
+        guard let writePath = NSURL(fileURLWithPath: path).appendingPathComponent(folder) else { return }
+        try? FileManager.default.createDirectory(atPath: writePath.path, withIntermediateDirectories: true)
+        let file = writePath.appendingPathComponent(fileNamed + ".txt")
+        try? text.write(to: file, atomically: false, encoding: String.Encoding.utf8)
+    }
+    func pause(){
+        self.pauseBeginning = Int(NSDate().timeIntervalSince1970)
+        self.enPause = true;
+    }
+    func getSiEnPause() -> Bool {
+        return self.enPause
+    }
+    func sendText() ->String{
+        return self.text;
+    }
+    func receiveText(_text:String) -> Void {
+        self.text = _text;
+        self.text += "\n" + "-" + getStrTime() + " : "
+    }
+    func adjustTime(_hours:Int, _minutes:Int, _seconds:Int){
+        self.timeBeginning = Int(NSDate().timeIntervalSince1970) - ((_hours * 3600) + (_minutes*60) + _seconds)
+        print(_minutes*60);
+    }
+    func tick(){
+        if !(self.enPause){
+            self.time = Int(NSDate().timeIntervalSince1970) - self.timeBeginning
+            //print(self.timeBeginning)
+            
+        }
+        if (self.enPause && !self.begin){
+            self.pauseTime = Int(NSDate().timeIntervalSince1970)
+            
+        }
+    }
+    
+}
+
+
